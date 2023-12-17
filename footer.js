ff8e1fb311d67c833ef49f696a18ab3a54a8d4cc
Victor Soto <135081103+FixedBits@users.noreply.github.com>
2023-12-17 06:34:56 -0700

    updated footer

--- footer.js
+++ footer.js
@@ -9,7 +9,7 @@ class Footer extends HTMLElement
             <div id="footer">
                 <p>
                     Copyright © 2023 <a href="https://www.bellevue.edu/">Bellevue University</a> | 
-                    <a href="https://github.com/""">GitHub</a> | 
+                    <a href="https://github.com/FixedBits">GitHub</a> | 
                     <a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/">Web Development Degree</a> | 
                     <a href="https://www.youtube.com/">YouTube Channel</a> | 
                     <a href="https://github.com/buwebdev/web-330">Course GitHub</a>
