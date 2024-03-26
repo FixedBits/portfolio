c5f38f5606161a20c1c02ba5cd88825f7fe26546
fixedbits <fixingbitbybit@gmail.com>
2024-03-26 00:54:17 -0600

    Update header.js

--- header.js
+++ header.js
@@ -13,30 +13,28 @@ class Header extends HTMLElement
             <div id="nav-container">
                 <ul id="nav-list">
                     <li>
-                        <a href="/index.html">Home</a>
+                        <a href="/portfolio/index.html">Home</a>
                     </li>
                     <li>
-                        <a href="/projects.html">Projects</a>
+                        <a href="/portfolio/projects.html">Projects</a>
                     </li>
                     <li>
-                        <a href="/database-diagrams.html">Database Diagrams</a>
+                        <a href="/portfolio/database-diagrams.html">Database Diagrams</a>
                     </li>
                     <li>
-                        <a href="/api-unit-tests.html">API Unit Tests</a>
+                        <a href="/portfolio/api-unit-tests.html">API Unit Tests</a>
                     </li>
                     <li>
-                        <a href="/resume.html">Resume</a>
+                        <a href="/portfolio/resume.html">Resume</a>
                     </li>
                     <li>
-                        <a href="/about.html">About</a>
+                        <a href="/portfolio/about.html">About</a>
                     </li>
                     <li>
-                        <a href="/web-430/soto-tech-value-stream.html">DevOps Presentations</a>
+                        <a href="/portfolio/web-430/soto-tech-value-stream.html">DevOps Presentations</a>
                     </li>
                 </ul>
             </div>
         `;
     }
 }
-
-customElements.define("header-component", Header);
